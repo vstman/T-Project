ef80b0693d666da01ae04a22486f9b55e4bcfdeb
Muhammed Aydın <muhammedaydiiinnn@gmail.com>
2024-07-10 17:13:10 +0300

    Admin Template Düzenlendi.

--- resources/views/adminPanel/layout/app.blade.php
+++ resources/views/adminPanel/layout/app.blade.php
@@ -5,12 +5,11 @@
     <link rel="icon" type="image/x-icon" href="{{asset('projectPanel/assets/favicon.ico')}}"/>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- CSS Files -->
-    <link rel="stylesheet" href="{{asset('adminPanel/assets/css/bootstrap.min.css')}}"/>
-    <link rel="stylesheet" href="{{asset('adminPanel/assets/css/kaiadmin.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('adminPanel/assets/css/style.min.css')}}"/>
 
     <!-- FontAwesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
@@ -20,12 +19,14 @@
             .sidebar {
                 display: none;
             }
+
             .sidebar.active {
                 display: block;
                 position: absolute;
                 width: 250px;
                 z-index: 1000;
             }
+
             .sidenav-toggled .sidebar {
                 display: block;
             }
@@ -77,9 +78,11 @@
                 <div class="container-fluid">
                     <ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
                         <li class="nav-item topbar-user dropdown hidden-caret">
-                            <a class="dropdown-toggle profile-pic" data-bs-toggle="dropdown" href="#" aria-expanded="false">
+                            <a class="dropdown-toggle profile-pic" data-bs-toggle="dropdown" href="#"
+                               aria-expanded="false">
                                 <div class="avatar-sm">
-                                    <img src="{{asset('adminPanel/assets/img/profile.jpg')}}" alt="..." class="avatar-img rounded-circle"/>
+                                    <img src="{{asset('adminPanel/assets/img/profile.jpg')}}" alt="..."
+                                         class="avatar-img rounded-circle"/>
                                 </div>
                                 <span class="profile-username">
                                     <span class="op-7">Merhaba,</span>
@@ -99,17 +102,21 @@
             </nav>
             <!-- End Navbar -->
         </div>
-        <div class="container">
-            <div class="container">
+
+        <div class="container mt-3">
+            <div class="container mt-3">
                 @yield('content')
             </div>
+
         </div>
+
     </div>
 </div>
 <!--   Core JS Files   -->
-<script src="{{asset('adminPanel/assets/js/core/jquery-3.7.1.min.js')}}"></script>
-<script src="{{asset('adminPanel/assets/js/core/popper.min.js')}}"></script>
-<script src="{{asset('adminPanel/assets/js/core/bootstrap.min.js')}}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js"></script>
 
 <script>
     // Sidebar toggle for small screens
