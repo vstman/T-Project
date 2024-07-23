381dd6c3604b9427a532e05890b4f1f67b27b448
Muhammed Aydın <muhammedaydiiinnn@gmail.com>
2024-07-23 14:08:17 +0300

    Detay Sayfasına veri çekildi

--- resources/views/adminPanel/detail.blade.php
+++ resources/views/adminPanel/detail.blade.php
@@ -3,67 +3,64 @@
 @section('content')
 
     <a href="{{ route('admin.index') }}" class="btn btn-warning">
-
         <i class="fas fa-arrow-left"></i> Geri
     </a>
     <br>
     <br>
 
-    <form>
+    <div class="container">
         <table class="table table-bordered">
             <tbody id="project-table-body">
             <tr>
                 <td><label for="supporting-organization" class="col-form-label">Proje Destekleyen Kurum:</label></td>
-                <td colspan="2"><input type="text" class="form-control" id="supporting-organization" name="supporting_organization"></td>
+                <td colspan="2"><p id="supporting-organization">{{ $post->supporting_organization }}</p></td>
             </tr>
             <tr>
                 <td><label for="project-title" class="col-form-label">Proje Adı ve kodu:</label></td>
-                <td><input class="form-control" id="project-title" name="project_title" placeholder="Proje Adı"></td>
-                <td><input class="form-control" id="project-code" name="project_code" placeholder="Kodu"></td>
+                <td><p id="project-title">{{ $post->project_title }}</p></td>
+                <td><p id="project-code">{{ $post->project_code }}</p></td>
             </tr>
             <tr>
                 <td><label for="supervisor" class="col-form-label">Yürütücü:</label></td>
                 <td>
                     <div class="d-flex flex-column">
                         <div class="d-flex align-items-center mb-2">
-                            <img id="supervisor-photo" src="image.png" alt="Supervisor Photo" class="img-thumbnail mr-2" width="100" height="100">
-                            <input type="file" class="form-control-file" id="photo-input" accept="image/*">
+                            <img id="supervisor-photo" src="{{ asset('path/to/default/image.png') }}" alt="Supervisor Photo" class="img-thumbnail mr-2" width="100" height="100">
+                            <!-- Fotoğraf yükleme işlemi burada değilse, varsayılan fotoğrafı gösterebilirsiniz -->
                         </div>
-                        <input type="text" class="form-control mb-2" id="supervisor" name="supervisor" placeholder="Unvan Ad Soyad">
+                        <p id="supervisor">{{ $post->supervisor }}</p>
                     </div>
                 </td>
-                <td><textarea class="form-control" id="department" name="department" rows="5" placeholder="Yürütücü Bölüm"></textarea></td>
+                <td><p id="department">{{ $post->department }}</p></td>
             </tr>
             <tr class="team-template">
                 <td>
                     <label for="team" class="col-form-label">Proje Ekibi:</label>
-
                 </td>
-
-
                 <td colspan="1">
                     <div class="row">
-                        <div class="col">
-                            <input class="form-control" name="team_name[]" placeholder="Ad Soyad">
-                        </div>
-                        <div class="col">
-                            <input class="form-control" name="team_position[]" placeholder="Görevi">
-                        </div>
+                        @foreach ($post->teamMembers as $teamMember)
+                            <div class="col">
+                                <p id="team_name">{{ $teamMember->name }}</p>
+                            </div>
+                            <div class="col">
+                                <p id="team_position">{{ $teamMember->position }}</p>
+                            </div>
+                        @endforeach
                     </div>
                 </td>
-                <td><input class="form-control" name="team_department[]" placeholder="Üniversite Bölüm">
-                </td>
+                <td><p id="team_department">{{ $teamMember->department ?? 'Belirtilmemiş' }}</p></td>
             </tr>
             <tr>
                 <td><label for="duration" class="col-form-label">Proje Süresi:</label></td>
-                <td colspan="2"><input type="text" class="form-control" id="duration" name="duration"></td>
+                <td colspan="2"><p id="duration">{{ $post->duration }}</p></td>
             </tr>
             <tr>
                 <td><label for="budget" class="col-form-label">Proje Bütçesi:</label></td>
-                <td colspan="2"><input type="text" class="form-control" id="budget" name="budget"></td>
+                <td colspan="2"><p id="budget">{{ $post->budget }}</p></td>
             </tr>
             </tbody>
         </table>
-    </form>
+    </div>
 
 @endsection
--- resources/views/projectPanel/posts/detail.blade.php
+++ resources/views/projectPanel/posts/detail.blade.php
@@ -2,10 +2,65 @@
 
 @section('content')
 
-    <h2>{{$post->project_title}}</h2>
-    {!! $post->supervisor !!}
+    <a href="{{ route('posts.main') }}" class="btn btn-warning">
+        <i class="fas fa-arrow-left"></i> Geri
+    </a>
     <br>
-    <p>Yayinlanma Tarihi : </p>
-    <p>{{ $post->created_at->format('Y-m-d') }}</p>
+    <br>
+
+    <div class="container">
+        <table class="table table-bordered">
+            <tbody id="project-table-body">
+            <tr>
+                <td><label for="supporting-organization" class="col-form-label">Proje Destekleyen Kurum:</label></td>
+                <td colspan="2"><p id="supporting-organization">{{ $post->supporting_organization }}</p></td>
+            </tr>
+            <tr>
+                <td><label for="project-title" class="col-form-label">Proje Adı ve kodu:</label></td>
+                <td><p id="project-title">{{ $post->project_title }}</p></td>
+                <td><p id="project-code">{{ $post->project_code }}</p></td>
+            </tr>
+            <tr>
+                <td><label for="supervisor" class="col-form-label">Yürütücü:</label></td>
+                <td>
+                    <div class="d-flex flex-column">
+                        <div class="d-flex align-items-center mb-2">
+                            <img id="supervisor-photo" src="{{ asset('path/to/default/image.png') }}" alt="Supervisor Photo" class="img-thumbnail mr-2" width="100" height="100">
+                            <!-- Fotoğraf yükleme işlemi burada değilse, varsayılan fotoğrafı gösterebilirsiniz -->
+                        </div>
+                        <p id="supervisor">{{ $post->supervisor }}</p>
+                    </div>
+                </td>
+                <td><p id="department">{{ $post->department }}</p></td>
+            </tr>
+            <tr class="team-template">
+                <td>
+                    <label for="team" class="col-form-label">Proje Ekibi:</label>
+                </td>
+                <td colspan="1">
+                    <div class="row">
+                        @foreach ($post->teamMembers as $teamMember)
+                            <div class="col">
+                                <p id="team_name">{{ $teamMember->name }}</p>
+                            </div>
+                            <div class="col">
+                                <p id="team_position">{{ $teamMember->position }}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                </td>
+                <td><p id="team_department">{{ $teamMember->department ?? 'Belirtilmemiş' }}</p></td>
+            </tr>
+            <tr>
+                <td><label for="duration" class="col-form-label">Proje Süresi:</label></td>
+                <td colspan="2"><p id="duration">{{ $post->duration }}</p></td>
+            </tr>
+            <tr>
+                <td><label for="budget" class="col-form-label">Proje Bütçesi:</label></td>
+                <td colspan="2"><p id="budget">{{ $post->budget }}</p></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 
 @endsection
