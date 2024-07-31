8abe6a61a2199b2820495a9a6500328bee661c5d
Onur Işık <onurisik5523@gmail.com>
2024-07-31 14:32:01 +0300

    Detay Sayfasinda Silme Butonu

--- resources/views/adminPanel/detail.blade.php
+++ resources/views/adminPanel/detail.blade.php
@@ -67,5 +67,53 @@
         <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-success">Düzenle</a>
         <button class="btn btn-danger" onclick="confirmDeletion('{{ route('admin.posts.destroy', $post->id) }}')">Sil</button>
     </div>
+<!-- SweetAlert2 -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <script>
+    
+    function confirmDeletion(url) {
+        Swal.fire({
+            title: 'Bu öğeyi silmek istediğinize emin misiniz?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, sil!',
+            cancelButtonText: 'Hayır'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: url,
+                    type: 'POST',
+                    data: {
+                        _method: 'DELETE',
+                        _token: '{{ csrf_token() }}'
+                    },
+                    success: function (result) {
+                        Swal.fire(
+                            'Silindi!',
+                            'Öğe başarıyla silindi.',
+                            'success'
+                        ).then(() => {
+                            window.location.href = '{{ route('admin.index') }}';
+                        });
+                    },
+                    error: function (result) {
+                        Swal.fire(
+                            'Hata!',
+                            'Öğe silinirken bir hata oluştu.',
+                            'error'
+                        );
+                    }
+                });
+            }
+        })
+    }
+</script>
 
 @endsection
