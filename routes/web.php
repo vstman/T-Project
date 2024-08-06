8fe23e9a400e4ae8b2a196d24752f74eb36669b8
Onur Işık <onurisik5523@gmail.com>
2024-08-06 16:36:17 +0300

    Route Duzenleme

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,6 @@ Route::get('/details/{id}', [PostController::class, 'details'])->name('posts.det
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
-
+Route::get('/register', function () {abort(404);});
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
