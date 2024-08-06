4b637db8f2ce3f4b5f6c51c0e594594fbfb33393
Muhammed Aydın <muhammedaydiiinnn@gmail.com>
2024-08-06 11:36:34 +0300

    Bug fix

--- resources/views/adminPanel/layout/app.blade.php
+++ resources/views/adminPanel/layout/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>Projelerimiz - Admin</title>
-    <link rel="icon" type="image/x-icon" href="{{asset('projectPanel/assets/favicon.ico')}}"/>
+    <link rel="icon" type="image/x-icon" href="{{ asset('projectPanel/assets/favicon.ico') }}"/>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="//cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
@@ -12,8 +12,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-
-    <link rel="stylesheet" href="{{asset('adminPanel/assets/css/kaiadmin.min.css')}}"/>
+    <link rel="stylesheet" href="{{ asset('adminPanel/assets/css/kaiadmin.min.css') }}"/>
 
     <!-- FontAwesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
@@ -26,7 +25,6 @@
             height: 200px;
             object-fit: cover; /* Resmi kutuya sığdırmak için */
         }
-
     </style>
 </head>
 <body>
@@ -36,13 +34,9 @@
         <div class="sidebar-logo">
             <!-- Logo Header -->
             <div class="logo-header" data-background-color="dark">
-                <a href="{{route('admin.index')}}" class="logo">
-                    <img
-                        src="{{asset('adminPanel/assets/img/MTÜ LOGO 4.png')}}"
-                        alt="navbar brand"
-                        class="navbar-brand"
-                        height="50"
-                    />
+                <a href="{{ route('admin.index') }}" class="logo">
+                    <img src="{{ asset('adminPanel/assets/img/MTÜ LOGO 4.png') }}"
+                         alt="navbar brand" class="navbar-brand" height="50"/>
                 </a>
                 <div class="nav-toggle">
                     <button class="btn btn-toggle toggle-sidebar">
@@ -52,7 +46,6 @@
                         <i class="gg-menu-left"></i>
                     </button>
                 </div>
-
             </div>
             <!-- End Logo Header -->
         </div>
@@ -60,27 +53,18 @@
             <div class="sidebar-content">
                 <ul class="nav nav-secondary">
                     <li class="nav-item submenu">
-                        <a
-                            data-bs-toggle="collapse"
-                            class="collapsed"
-                            aria-expanded="false"
-                        >
+                        <a data-bs-toggle="collapse" href="#userCollapse" class="collapsed" aria-expanded="false">
                             <i class="fas fa-user"></i>
-                            <p>Hoşgeldin, Kullanıcı</p>
+                            <p>Hoşgeldin, {{ Auth::user()->name }}</p>
                         </a>
                     </li>
                     <br>
                     <li class="nav-item submenu">
-                        <a
-                            data-bs-toggle="collapse"
-                            href="#collapseExample"
-                            class="collapsed"
-                            aria-expanded="false"
-                        >
+                        <a data-bs-toggle="collapse" href="#projectCollapse" class="collapsed" aria-expanded="false">
                             <i class="fas fa-project-diagram"></i>
                             <p>Projeler</p>
                         </a>
-                        <div id="collapseExample" class="collapse">
+                        <div id="projectCollapse" class="collapse">
                             <ul class="nav nav-collapse">
                                 <li>
                                     <a href="{{ route('admin.posts.create') }}">Yeni Proje Ekle</a>
@@ -88,14 +72,27 @@
                                 <li>
                                     <a href="{{ route('admin.index') }}">Projeleri Yönet</a>
                                 </li>
-                                
                             </ul>
                         </div>
                     </li>
                     <br>
-                    <li>
-                        <a href="{{ route('admin.users.create') }}"  class="nav-item menu">Yeni Kullanıcı Ekle</a>
+                    <li class="nav-item submenu">
+                        <a data-bs-toggle="collapse" href="#userManageCollapse" class="collapsed" aria-expanded="false">
+                            <i class="fas fa-users"></i>
+                            <p>Kullanıcılar</p>
+                        </a>
+                        <div id="userManageCollapse" class="collapse">
+                            <ul class="nav nav-collapse">
+                                <li>
+                                    <a href="{{ route('admin.users.index') }}">Kullanıcılar</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.users.create') }}">Yeni Kullanıcı Ekle</a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
+                    <br>
                     <li class="nav-item submenu">
                         <form id="navigate-form" action="{{ route('posts.main') }}" method="GET" style="display: none;" target="_blank">
                             @csrf
@@ -105,15 +102,12 @@
                             <p>Siteye Git</p>
                         </a>
                     </li>
-
-
                     <br>
                     <li class="nav-item submenu">
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
-                        <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                           class="collapsed" aria-expanded="false">
+                        <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="collapsed" aria-expanded="false">
                             <i class="fas fa-sign-out-alt"></i>
                             <p>Çıkış Yap</p>
                         </a>
@@ -133,12 +127,7 @@
                         <button class="btn btn-toggle toggle-sidebar">
                             <i class="gg-menu-right"></i>
                         </button>
-                        <img
-                            src="{{asset('adminPanel/assets/img/MTÜ LOGO 4.png')}}"
-                            alt="navbar brand"
-                            class="navbar-brand"
-                            height="50"
-                        />
+                        <img src="{{ asset('adminPanel/assets/img/MTÜ LOGO 4.png') }}" alt="navbar brand" class="navbar-brand" height="50"/>
                         <button class="btn btn-toggle sidenav-toggler">
                             <i class="gg-menu-left"></i>
                         </button>
@@ -155,42 +144,41 @@
         </div>
     </div>
 </div>
-<!--   Core JS Files   -->
 
+<!--   Core JS Files   -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js"></script>
-<!--   Core JS Files   -->
-<script src="{{asset('adminPanel/assets/js/core/jquery-3.7.1.min.js')}}"></script>
-<script src="{{asset('adminPanel/assets/js/core/popper.min.js')}}"></script>
-<script src="{{asset('adminPanel/assets/js/core/bootstrap.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/core/jquery-3.7.1.min.js') }}"></script>
+<script src="{{ asset('adminPanel/assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('adminPanel/assets/js/core/bootstrap.min.js') }}"></script>
 
 <!-- jQuery Scrollbar -->
-<script src="{{asset('adminPanel/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
 
 <!-- Chart JS -->
-<script src="{{asset('adminPanel/assets/js/plugin/chart.js/chart.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/chart.js/chart.min.js') }}"></script>
 
 <!-- jQuery Sparkline -->
-<script src="{{asset('adminPanel/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
 
 <!-- Datatables -->
-<script src="{{asset('adminPanel/assets/js/plugin/datatables/datatables.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/datatables/datatables.min.js') }}"></script>
 
 <!-- Bootstrap Notify -->
-<script src="{{asset('adminPanel/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
 
 <!-- jQuery Vector Maps -->
-<script src="{{asset('adminPanel/assets/js/plugin/jsvectormap/jsvectormap.min.js')}}"></script>
-<script src="{{asset('adminPanel/assets/js/plugin/jsvectormap/world.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/jsvectormap/jsvectormap.min.js') }}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/jsvectormap/world.js') }}"></script>
 
 <!-- Sweet Alert -->
-<script src="{{asset('adminPanel/assets/js/plugin/sweetalert/sweetalert.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
 
-<script src="{{asset('adminPanel/assets/js/kaiadmin.min.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/kaiadmin.min.js') }}"></script>
 
-<script src="{{asset('adminPanel/assets/js/setting-demo.js')}}"></script>
-<script src="{{asset('adminPanel/assets/js/demo.js')}}"></script>
+<script src="{{ asset('adminPanel/assets/js/setting-demo.js') }}"></script>
+<script src="{{ asset('adminPanel/assets/js/demo.js') }}"></script>
 <script>
     // Sidebar toggle for small screens
     $(document).ready(function () {
@@ -205,14 +193,12 @@
         // Dropdown menu toggle for small screens
         $('.dropdown-toggle').dropdown();
     });
-</script>
-<script>
+
     function openFormInNewTab() {
         var form = document.getElementById('navigate-form');
         form.target = '_blank';
         form.submit();
     }
 </script>
-
 </body>
 </html>
--- resources/views/adminPanel/login.blade.php
+++ resources/views/adminPanel/login.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="tr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Giriş Yap</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
+@extends('projectPanel.layout.app')
+@section('style_content')
+@endsection
+@section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
@@ -29,16 +24,17 @@
                                 <label for="email">E-posta</label>
                                 <input type="email" id="email" class="form-control" name="email" required autofocus>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <label for="password">Şifre</label>
                                 <input type="password" id="password" class="form-control" name="password" required>
                             </div>
-                            <button type="submit" class="btn btn-primary">Giriş Yap</button>
+                            <div class="form-group mt-3 text-center">
+                                <button type="submit" class="btn btn-primary">Giriş Yap</button>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
