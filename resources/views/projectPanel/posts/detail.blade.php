07753e668842a327d3a3a9f3f587d391c25c5403
Muhammed Aydın <muhammedaydiiinnn@gmail.com>
2024-08-06 15:56:46 +0300

    Bug fix

--- resources/views/projectPanel/posts/detail.blade.php
+++ resources/views/projectPanel/posts/detail.blade.php
@@ -12,37 +12,40 @@
         <table class="table table-bordered">
             <tbody id="project-table-body">
             <tr>
-                <td class="narrow-column"><label for="supporting-organization" class="col-form-label">Proje Destekleyen Kurum:</label></td>
+                <td><label for="supporting-organization" class="col-form-label">Proje Destekleyen Kurum:</label></td>
                 <td colspan="2"><p id="supporting-organization">{{ $post->supporting_organization }}</p></td>
             </tr>
             <tr>
-                <td class="narrow-column"><label for="project-title" class="col-form-label">Proje Adı ve Kodu:</label></td>
-                <td colspan="2"><p>{{ $post->project_title }} - {{ $post->project_code }}</p></td>
+                <td><label for="project-title" class="col-form-label">Proje Adı ve kodu:</label></td>
+                <td colspan="2"><p id="project-title">{{ $post->project_title }} - {{ $post->project_code }}</p></td>
             </tr>
-            <tr>
-                @if ($post->supervisors->isNotEmpty())
-                    <tr>
-                        <td><label for="supervisors" class="col-form-label">Yürütücüler:</label></td>
-                        <td colspan="2">
-                            @foreach ($post->supervisors as $supervisor)
-                                <div class="d-flex align-items-center mb-2">
-                                    <img src="{{ asset($supervisor->supervisor_photo ?? 'default.png') }}" alt="Supervisor Photo" class="img-thumbnail mr-2" width="100" height="100">
-                                    <div>
-                                        <p><strong>Ad:</strong> {{ $supervisor->name }}</p>
-                                        <p><strong>Bölüm:</strong> {{ $supervisor->department }}</p>
-                                    </div>
+            @if ($post->supervisors->isNotEmpty())
+                <tr>
+                    <td rowspan="{{$post->supervisors->count()}}">
+                        <label for="supervisors" class="col-form-label">Yürütücüler:</label>
+                    </td>
+                    @foreach ($post->supervisors as $supervisor)
+                        <td colspan="1">
+                            <div class="d-flex flex-column align-items-center">
+                                <img src="{{ asset($supervisor->supervisor_photo ?? 'default.png') }}"
+                                     alt="Supervisor Photo"
+                                     class="img-thumbnail mr-2 fixed-size supervisor-photo-preview" width="100"
+                                     height="100">
+                                <div>
+                                    <p><strong>Ad:</strong> {{ $supervisor->name }}</p>
                                 </div>
-                            @endforeach
+                            </div>
                         </td>
-                    </tr>
-                @endif
 
-                <td><p id="department">{{ $post->department }}</p></td>
-            </tr>
+                        <td><p><strong>Üniversite - Bölüm:</strong> {{ $supervisor->department }}</p></td>
+                </tr>
+                @endforeach
+            @endif
+
 
             @if ($post->teamMembers->isNotEmpty())
                 <tr class="team-template">
-                    <td rowspan="{{ $post->teamMembers->count() }}" class="narrow-column">
+                    <td rowspan="{{ $post->teamMembers->count() }}">
                         <label for="team" class="col-form-label">Proje Ekibi:</label>
                     </td>
                     @foreach ($post->teamMembers as $teamMember)
@@ -57,11 +60,11 @@
             @endif
 
             <tr>
-                <td class="narrow-column"><label for="duration" class="col-form-label">Proje Süresi:</label></td>
+                <td><label for="duration" class="col-form-label">Proje Süresi:</label></td>
                 <td colspan="2"><p id="duration">{{ $post->duration }} Ay</p></td>
             </tr>
             <tr>
-                <td class="narrow-column"><label for="budget" class="col-form-label">Proje Bütçesi:</label></td>
+                <td><label for="budget" class="col-form-label">Proje Bütçesi:</label></td>
                 <td colspan="2"><p id="budget">{{ $post->budget }}</p></td>
             </tr>
             </tbody>
